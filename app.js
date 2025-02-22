4a22982d43d573dd263666194e6f764e937861ad
Christopher <christopherjocizz@gmail.com>
2025-02-22 01:48:01 -0500

    update

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
-function calcular (a, b, operación){
-    switch operación {
-        case 'suma':
-            return a + b;
-    switch operación {
-        case 'suma':
-            
-    switch operación {
-        case 'suma':
-        
-                        
+function calcular (a, b, operacion) {
+    switch (operacion) {
+      case 'suma':
+        return a + b;
+      case 'resta':
+        return a - b;
+      case 'multiplicacion':
+        return a * b;
+      case 'division':
+        return b !== 0 ? a/b : 'no se puede dividir por 0';
+      default:
+        return 'la operacion debe se o una +, -, *, /';
     }
 }
-console.log ("operación")
+console.log (calcular (10, 1, 'suma'));
