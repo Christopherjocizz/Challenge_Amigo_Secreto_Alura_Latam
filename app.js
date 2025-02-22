4bd5392c3950439b124b9cc6ecb999b0d87f9524
Christopher <christopherjocizz@gmail.com>
2025-02-22 00:55:06 -0500

    update

--- app.js
+++ app.js
@@ -2,6 +2,13 @@ function calcular (a, b, operación){
     switch operación {
         case 'suma':
             return a + b;
+    switch operación {
+        case 'suma':
+            
+    switch operación {
+        case 'suma':
+        
+                        
     }
 }
 console.log ("operación")
